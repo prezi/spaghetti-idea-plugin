1fb26cf4ba34c766aa303da006a6ab782c4300f1
Balint Gabor <balint@prezi.com>
2014-11-06 17:34:19 +0100

    Updated template code

--- src/com/prezi/intellij/spaghetti/SpaghettiModuleTokenType.java
+++ src/com/prezi/intellij/spaghetti/SpaghettiModuleTokenType.java
@@ -11,6 +11,6 @@ public class SpaghettiModuleTokenType extends IElementType {
 
     @Override
     public String toString() {
-        return "SimpleTokenType." + super.toString();
+        return "SpaghettiModuleTokenType." + super.toString();
     }
 }
